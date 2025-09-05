c43820f5a29c1f97f0d0e1c0a17479c5cc053f01
Garv Raj <154749848+GitWithGarv@users.noreply.github.com>
2025-09-05 17:50:44 +0000

    Coping the array int another array

---
+++ arrays/copingArray.java
@@ -0,0 +1,23 @@
+public class copingArray {
+    public static void main(String[] args) {
+        int a[] = {1,2,3,4,5,6,7,8,9,10};
+        int b[] = new int[a.length];
+
+        System.out.print("Array a : ");
+        for(int num : a){
+            System.out.print(num + "\t");
+        }
+        System.out.println("");
+
+        for(int i = 0 ; i < b.length ; i++){
+            b[i] = a[i];
+        }
+
+        System.out.print("Copied Array b : ");
+
+
+        for(int num : b){
+            System.out.print(num +"\t");
+        }
+    }
+}
