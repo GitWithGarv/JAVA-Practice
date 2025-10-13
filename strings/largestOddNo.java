f8ca59c6218fdc4a4ceb1c54aaf187157f0dfc67
Garv Raj <154749848+GitWithGarv@users.noreply.github.com>
2025-10-13 18:04:33 +0000

    finding the largest odd no. in a given string

---
+++ strings/largestOddNo.java
@@ -0,0 +1,19 @@
+package strings;
+
+public class largestOddNo {
+
+    public static String largestOdd(String s){
+        for(int i = s.length()-1 ; i >= 0; i--){
+            char c = s.charAt(i);
+            if((c-'0')%2 == 1){
+                return s.substring(0, i+1);
+            }
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String str = "35427";
+        System.out.println("The largest odd no. of the given string is : "+largestOdd(str));
+    }
+}
